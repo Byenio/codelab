1d234b1a36d7f614e81ca39057571d082cb12495
Byenio <marcel.bienko@racing.polsl.pl>
2026-03-06 21:07:21 +0100

    feat(backend): update middleware with auth, protect routes

--- backend/include/middleware/auth_middleware.h
+++ backend/include/middleware/auth_middleware.h
@@ -4,6 +4,7 @@
 #include <crow.h>
 #include "models/user.h"
 #include "dao/user_dao.h"
+#include "services/auth_service.h"
 
 namespace codelab::middleware
 {
@@ -11,12 +12,25 @@ namespace codelab::middleware
   {
     struct context
     {
-      models::User current_user;
+      int user_id = 0;
     };
 
     void before_handle(crow::request& req, crow::response& res, context& ctx)
     {
+      std::string auth_header = req.get_header_value("Authorization");
+      if (auth_header.empty()) return;
 
+      if (auth_header.substr(0, 7) != "Bearer ") return;
+
+      std::string token = auth_header.substr(7);
+
+      services::AuthService auth;
+      auto uid = auth.VerifyToken(token);
+
+      if (uid)
+      {
+        ctx.user_id = *uid;
+      }
     }
 
     void after_handle (crow::request& req, crow::response& res, context& ctx)
